0b96b4eb6600213a8fec9c108b33b2dbaf6e7b66
Mauro Chumpitaz Polino <mchumpitazp@gmail.com>
2023-02-06 12:51:46 +0300

    Header done

---
+++ beauty-equipment-client/src/components/HeaderComponent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Navbar, NavbarBrand, Nav, NavItem } from 'reactstrap';
+
+function Header () {
+    return (
+        <Container>
+            <Navbar>
+                <NavbarBrand href='/'>
+                    <img src="/logo.png" height='50px' alt="logo" />
+                </NavbarBrand>
+
+                <Nav navbar className='flex-row'>
+                    <NavItem className='d-none d-sm-block'>
+                            <i className='bi-envelope'></i>
+                            <span className='ms-2'>info@be.com</span>
+                    </NavItem>
+                    <NavItem className='ms-4'>
+                            <i className='bi-telephone-outbound'></i>
+                            <span className='ms-2'>8 800 000 00 01</span>
+                    </NavItem>
+                </Nav>
+            </Navbar>
+        </Container>
+    );
+}
+
+export default Header;
---
+++ beauty-equipment-client/src/components/MainComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from './HeaderComponent';
+import Headline from "./HeadlineComponent";
+
+function Main () {
+    return (
+        <Header />
+    );
+}
+
+export default Main;
